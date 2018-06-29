48d265b61b93b9943ecf358436b7a859843d0e28
Marco Mark <marco@silvanite.com>
2018-06-29 10:50:33 +0100

    Base Laravel Package to publish to packagist

---
+++ src-php/Providers/PackageServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Maxfactor\CMS\Providers;
+
+class PackageServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+    /**
+     * Load custom views
+     *
+     * @return void
+     */
+    private function bootViews()
+    {
+        //
+    }
+}
