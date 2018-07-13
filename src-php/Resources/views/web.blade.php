3f6de7830745d26dd65ec77819eb74af9e97ecbf
Marco Mark <marco@silvanite.com>
2018-07-13 14:56:13 +0100

    Include a default web route and layout to show that everything is installed and working

---
+++ src-php/Resources/views/web.blade.php
@@ -0,0 +1,15 @@
+@extends('maxfactor::layouts.default')
+
+@section('browser_title', 'Maxfactor CMS')
+
+@section('content')
+    <section class="authentication">
+
+        <h1>@lang('Welcome to Maxfactor CMS')</h1>
+
+        <div>
+            Have a wonderful life and may you be very happy always, thank you.
+        </div>
+
+    </section>
+@endsection
