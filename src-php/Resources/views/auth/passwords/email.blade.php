f857ac8b66fa5908cf6e2bade4c187a812ae4a11
Jacob Walters <jacob.walters@dewsign.co.uk>
2018-07-16 17:06:53 +0100

    update markup to make styling and targeting elements easier

--- src-php/Resources/views/auth/passwords/email.blade.php
+++ src-php/Resources/views/auth/passwords/email.blade.php
@@ -3,26 +3,28 @@
 @section('browser_title', 'Reset Password')
 
 @section('content')
-    <section class="authentication">
+    <section class="authentication authentication--email">
+        <div class="authentication__panel">
 
-        <h1>@lang('Send Password Reset')</h1>
+            <h1>@lang('Send Password Reset')</h1>
 
-        <div class="form">
-            <form method="POST" action="{{ route('password.email') }}">
-                {{ csrf_field() }}
+            <div class="form">
+                <form method="POST" action="{{ route('password.email') }}">
+                    {{ csrf_field() }}
 
-                @include('maxfactor::partials.form.email')
+                    @include('maxfactor::partials.form.email')
 
-                <div class="form__field">
-                    <button type="submit" class="button button--primary">
-                        @lang('Send')
-                    </button>
-                </div>
+                    <div class="form__field">
+                        <button type="submit" class="button button--primary">
+                            @lang('Send')
+                        </button>
+                    </div>
 
-                @include('maxfactor::partials.form.status')
-                @include('maxfactor::partials.form.message')
-            </form>
+                    @include('maxfactor::partials.form.status')
+                    @include('maxfactor::partials.form.message')
+                </form>
+            </div>
+
         </div>
-
     </section>
 @endsection
